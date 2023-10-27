ac959b079d067bdce061dccbb112eb52288dbab5
ChaseCoble <113311559+ChaseCoble@users.noreply.github.com>
2023-10-27 10:08:44 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [allItems, setAllItems] = useState({});
+  useEffect(() => {
+    const fetchAll = async () => {
+      try{
+        const response = await fetch("localhost:5000/get", {
+          method: "GET",
+          mode: "cors",
+          cache: "no-cache"
+        });
+        const data = await response.json();
+        setAllItems(data);
+      }catch (error){
+        console.log(error)
+      }
+    }
+    fetchAll();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is merely a data reciever
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>{allItems}</p>
     </div>
   );
 }
